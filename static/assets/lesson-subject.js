13ed573e31746cbcd0507cbaa7609145743363ba
Colin Chan <colin+git@lumeh.org>
2024-06-03 20:09:08 -0700

    Re-write lesson subject code
    Copied from https://github.com/csmenlopark/www/commit/91c557a286a6d3a22b835466ce13d51c7d30803a

--- static/assets/lesson-subject.js
+++ static/assets/lesson-subject.js
@@ -1,5 +1,30 @@
-window.addEventListener("load", function() {
-  var subjects = [
+document.addEventListener("DOMContentLoaded", (e) => {
+  const subjects = [
+    "God",
+    "Sacrament",
+    "Life",
+    "Truth",
+    "Love",
+    "Spirit",
+    "Soul",
+    "Mind",
+    "Christ Jesus",
+    "Man",
+    "Substance",
+    "Matter",
+    "Reality",
+    "Unreality",
+    "Are Sin, Disease, and Death Real?",
+    "Doctrine of Atonement",
+    "Probation After Death",
+    "Everlasting Punishment",
+    "Adam and Fallen Man",
+    "Mortals and Immortals",
+    "Soul and Body",
+    "Ancient and Modern Necromancy, alias Mesmerism and Hypnotism, Denounced",
+    "God the Only Cause and Creator",
+    "God the Preserver of Man",
+    "Is the Universe, Including Man, Evolved by Atomic Force?",
     "Christian Science",
     "God",
     "Sacrament",
@@ -25,12 +50,23 @@ window.addEventListener("load", function() {
     "Ancient and Modern Necromancy, alias Mesmerism and Hypnotism, Denounced",
     "God the Only Cause and Creator",
     "God the Preserver of Man",
-    "Is the Universe, Including Man, Evolved by Atomic Force?"
+    "Is the Universe, Including Man, Evolved by Atomic Force?",
+    "Christian Science",
+    "Christ Jesus (probably? it's a 53rd Sunday)",
   ];
-  var startDate = moment("2023-12-25");
-  var weeksSinceStart = moment().diff(startDate, "weeks");
-  var subjectIndex = weeksSinceStart % subjects.length;
-  var currentSubject = subjects[subjectIndex];
+  const now = new Date();
+  const nextSunday = getNextSunday(new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate()+6)));
+  const yearFirstSunday = getNextSunday(new Date(Date.UTC(nextSunday.getUTCFullYear(), 0, 1)));
+  const weekMillis = 1000 * 60 * 60 * 24 * 7;
+  const sundayNumber = Math.floor((nextSunday - yearFirstSunday) / weekMillis);
+  const currentSubject = subjects[sundayNumber];
   var subjectSpan = document.getElementById("bible-lesson-subject");
   subjectSpan.innerHTML = currentSubject;
 });
+
+function getNextSunday(d) {
+  if (d.getUTCDay() != 0) {
+    d.setUTCDate(d.getUTCDate() + (7 - d.getUTCDay()));
+  }
+  return d;
+}
